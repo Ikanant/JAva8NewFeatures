843f1a9e187686679a58f4096443d4728c0f3f16
Ikanant <jonathan.hdez92@gmail.com>
2019-03-02 11:18:11 -0500

    Transformed anonymous function into lambda expression

--- LambdaExpressions/src/FirstLambda.java
+++ LambdaExpressions/src/FirstLambda.java
@@ -6,11 +6,22 @@ public class FirstLambda {
 		
 		FileFilter fileFilter = new FileFilter() {
 			@Override
-			public boolean accept (File pathName) {
-				return pathName.getName().endsWith(".java");
+			public boolean accept (File file) {
+				return file.getName().endsWith(".java");
 			}
 		};
 		
+		File dir = new File("E:\\Workspaces\\Java\\Java8NewFeatures\\LambdaExpressions\\src");
+		File [] filesFromAnonymous = dir.listFiles(fileFilter); // <----- using anonymous function.
 		
+		FileFilter fileFilter2 = (File file) -> file.getName().endsWith(".java");
+		File [] filesFromLambda = dir.listFiles(fileFilter2); // <----- using Lambda epxression
+		
+		for (File f : filesFromAnonymous) {
+			System.out.println(f);
+		}
+		for (File f : filesFromLambda) {
+			System.out.println(f);
+		}
 	}
 }
