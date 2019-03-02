d899c512ae70e28c2f7b9760878b98e465c3c5fd
Ikanant <jonathan.hdez92@gmail.com>
2019-03-02 11:29:42 -0500

    Converted Runnable implementation for the thread into a lambdaExpression

--- LambdaExpressions/src/RunnableLambda.java
+++ LambdaExpressions/src/RunnableLambda.java
@@ -11,8 +11,18 @@ public class RunnableLambda {
 			}
 		};
 		
+		Runnable runnableLambda = () -> {
+			for (int i=0; i<3; i++) {
+				System.out.println("Hello from thread: " + Thread.currentThread().getName() + ".");
+			}
+		};
+		
 		Thread t = new Thread(runnable);
 		t.start();
 		t.join(); // This will throw a InterruptedException.. hence the throws above
+		
+		Thread t2 = new Thread(runnableLambda);
+		t2.start();
+		t2.join(); // This will throw a InterruptedException.. hence the throws above
 	}
 }
