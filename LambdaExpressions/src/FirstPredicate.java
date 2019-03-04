65ba07a5797b120166620fe647a914ad75ea8a68
Ikanant <jonathan.hdez92@gmail.com>
2019-03-03 22:24:07 -0500

    Start using Streams and predicates together

---
+++ LambdaExpressions/src/FirstPredicate.java
@@ -0,0 +1,16 @@
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class FirstPredicate {
+	public static void main(String... args) {
+		// Stream of strings
+		Stream<String> stream = Stream.of("one", "two", "three", "four", "five");
+		
+		Predicate<String> p1 = s -> s.length() > 3;
+		Predicate<String> p2 = Predicate.isEqual("two");
+		
+		stream
+			.filter(p1.or(p2))
+			.forEach(System.out::println);
+	}
+}
